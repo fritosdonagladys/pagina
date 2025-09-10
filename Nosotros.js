096b220bd0c64470170cbd53474fdccf0f4896ce
fritosdonagladys <fernandobaquerob@gmail.com>
2025-09-09 22:00:56 -0500

    Add files via upload

---
+++ Nosotros.js
@@ -0,0 +1,21 @@
+const iconoMenu = document.querySelector('.iconoMenuImg');
+const miniMenu = document.querySelector('.nav');
+const cerrarMenu = document.querySelector('.cerrarMenu');
+
+iconoMenu.addEventListener('click', () => {
+    console.log("hola");
+    
+    miniMenu.style.transition = `ease-in-out 800ms all`;
+    miniMenu.style.top ="0px";
+    iconoMenu.style.transition = `ease-in-out 500ms all`;
+    iconoMenu.style.opacity ="0";
+})
+
+cerrarMenu.addEventListener('click', () => {
+    console.log("hola");
+    
+    miniMenu.style.transition = `ease-in 800ms all`;
+    miniMenu.style.top ="-1600px";
+    iconoMenu.style.transition = `ease-in-out 1000ms all`;
+    iconoMenu.style.opacity ="1";
+})

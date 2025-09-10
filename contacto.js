cdf360e502bafa46f24758b5c6b99a5bacc0a0c4
fritosdonagladys <fernandobaquerob@gmail.com>
2025-09-10 15:03:07 -0500

    Add files via upload

---
+++ contacto.js
@@ -0,0 +1,53 @@
+const iconoMenu = document.querySelector('.iconoMenuImg');
+const miniMenu = document.querySelector('.nav');
+const cerrarMenu = document.querySelector('.cerrarMenu');
+
+iconoMenu.addEventListener('click', () => {
+    console.log("hola");
+    
+    miniMenu.style.transition = `ease-in-out 800ms all`;
+    miniMenu.style.top ="0px";
+    iconoMenu.style.transition = `ease-in-out 500ms all`;
+    iconoMenu.style.opacity ="0";
+})
+
+cerrarMenu.addEventListener('click', () => {
+    console.log("hola");
+    
+    miniMenu.style.transition = `ease-in 800ms all`;
+    miniMenu.style.top ="-1600px";
+    iconoMenu.style.transition = `ease-in-out 1000ms all`;
+    iconoMenu.style.opacity ="1";
+})
+
+
+function enviar(){
+var nombre = document.getElementById('nombre').value;
+var correo = document.getElementById('email').value;
+var celular  = document.getElementById('whatsapp').value;
+var mensaje  = document.getElementById('mensaje').value;
+
+var celNumero = "+573154882038"
+
+var url = "https://wa.me/" + celNumero + "?text=" 
+    +"*Nombre :*" +nombre+"%0a"
+    +"*E-mail :* " +correo+"%0a"
+    +"*Whatsapp :* " +celular+"%0a"
+    +"*Mensaje :* " +mensaje+"%0a"
+    +"!Muchas gracias!";
+
+    window.open(url, '_blank').focus();
+
+}
+
+function icono(){
+
+    var celNumero = "+573154882038"
+
+    var url = "https://wa.me/" + celNumero + "?text="
+    +"¡Buenos días!";
+
+    window.open(url, '_blank').focus();
+
+}
+
